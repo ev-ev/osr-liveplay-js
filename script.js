737759a5db8a611263d950fb5d7823fd825cc3cc
Evgeny V <evgeny.singapore@gmail.com>
2023-11-18 20:44:58 +0800

    Made approach circle work, made playfield in the center and have a border of free space around it, made playfield scale with any size of canvas, fixed circle sizing to work like in stable, added map AR into, attempted (and failed) to make combo tint. (+4)

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var abs=Math.abs;
 var ready=0;
 var CURSORSZ = 70;
+var ACSZ = 4;
+
+var playfieldScale = 1;
+var playfieldTransformX = 0;
+var playfieldTransformY = 0;
 
 var displayText = '';
 
@@ -8,6 +13,7 @@ var replayData = []; //TimeMS x y keypressed
 var replayIdx = 0;
 var replayStartTime = 0;
 var replayTimeSum = 0;
+var replayCombo = -1;
 var songStart;
 
 var mapHit = [];
@@ -16,8 +22,10 @@ var mapIn = [];
 var mapOD3 = 80;
 var mapOD1 = 140;
 var mapOD5 = 200;
+var mapCSr = 54.4;
 var mapARpreempt = 1200;
 var mapARfull = 800;
+var comboColorCount = 1;
 
 var assets = {};
 
@@ -46,6 +54,14 @@ function setup(){
     var canvas = document.getElementById('canvas');
     w = canvas.getBoundingClientRect().width;
     h = canvas.getBoundingClientRect().height;
+    //osu playfield should be 512x384, figure out scale and transform
+    if (w > h) { 
+        playfieldScale = (h-56)/384;
+    } else {
+        playfieldScale = (w-64)/512;
+    }
+    playfieldTransformX = (w - 512*playfieldScale)/2;
+    playfieldTransformY = (h - 384*playfieldScale)/2;
     c = canvas.getContext('2d');
     
     
@@ -54,6 +70,7 @@ function setup(){
     loadImage('skins/yugen/cursor@2x.png').then(cursorImage => { assets['cursorImage'] = cursorImage;
     loadImage('skins/yugen/hitcircle@2x.png').then(hitCircleImage => { assets['hitCircle'] = hitCircleImage;
     loadImage('skins/yugen/hitcircleoverlay@2x.png').then(hitCircleOverlayImage => { assets['hitCircleOverlay'] = hitCircleOverlayImage;
+    loadImage('skins/yugen/approachcircle.png').then(approachCircleImage => { assets['approachCircle'] = approachCircleImage;
     loadReplay().then(replayValues => {
     replayData = replayValues['replayData'];
     replayTimeSum = replayValues['replayTimeSum'];
@@ -64,7 +81,9 @@ function setup(){
     mapOD3 = 80 - 6 * mapValues['Difficulty']['OverallDifficulty'];
     mapOD1 = 140 - 8 * mapValues['Difficulty']['OverallDifficulty'];
     mapOD5 = 200 - 10 * mapValues['Difficulty']['OverallDifficulty'];
+    mapCSr = (54.4 - 4.48 * mapValues['Difficulty']['CircleSize'])*playfieldScale*2; //mul by 2 since its the radius
     var mapAR = Number(mapValues['Difficulty']['ApproachRate']);
+    //var mapAR = 1;
     if (mapAR <= 5){
         mapARpreempt = 1200 + 600 * (5 - mapAR) / 5;
         mapARfull = 800 + 400 * (5 - mapAR) / 5;
@@ -72,6 +91,23 @@ function setup(){
         mapARpreempt = 1200 - 750 * (mapAR - 5) / 5;
         mapARfull = 800 - 500 * (mapAR - 5) / 5;
     }
+    var comboColors = [];
+    for (var key in mapValues['Colours']){
+        if (key.slice(0,5) === "Combo"){
+            comboColors[Number(key.slice(5))-1] = 'rgba('+mapValues['Colours'][key]+', 1.0)';
+        }
+    }
+    comboColorCount = comboColors.length;
+    promises = [];
+    for (var i = 0; i < comboColors.length; i++) {
+        promises.push(tintImage(hitCircleImage, comboColors[i]));
+    }
+    //console.log(promises);
+    Promise.all(promises).then(comboColorResults => {
+    for (var i = 0; i < comboColorResults.length; i++) {
+        assets['hitCircle'+i] = comboColorResults[i];
+    }
+    console.log(assets);
     var songPath = replayValues['folder-src']+mapValues['General']['AudioFilename'];
     loadSong(songPath).then(()=>{
     console.log(audioBuffers);
@@ -88,26 +124,36 @@ function setup(){
     });
     });
     });
-    
+    });
+    });
     
     
     renderer = setInterval(function() { render(); },10);
     
 }
 
-function scaleX(x){ return ( (x) / 512 ) * w; } //or 512x384 idk
-function scaleY(y){ return ( (y) / 384 ) * h; }
+//function scaleX(x){ return ( (x) / 512 ) * w; } //or 512x384 idk
+//function scaleY(y){ return ( (y) / 384 ) * h; }
+
+function renderX(x) { return x*playfieldScale+playfieldTransformX;}
+function renderY(y) { return y*playfieldScale+playfieldTransformY;}
 
 function drawCursor(x, y) {
     //console.log(scaleX(x),scaleY(y));
-    c.drawImage(assets['cursorImage'], scaleX(x) - CURSORSZ / 2, scaleY(y) - CURSORSZ / 2,CURSORSZ,CURSORSZ);
+    c.drawImage(assets['cursorImage'], renderX(x) - CURSORSZ / 2, renderY(y) - CURSORSZ / 2,CURSORSZ,CURSORSZ);
 }
 
-function drawHitCircle(x, y, opacity){
-    HITCIRCLESZ = 70;
+function drawHitCircle(x, y, image, opacity, current, timing){
+    var posX = renderX(x) - mapCSr / 2;
+    var posY = renderY(y) - mapCSr / 2;
     c.globalAlpha = opacity;
-    c.drawImage(assets['hitCircle'], scaleX(x) - HITCIRCLESZ / 2, scaleY(y) - HITCIRCLESZ / 2,HITCIRCLESZ,HITCIRCLESZ);
-    c.drawImage(assets['hitCircleOverlay'], scaleX(x) - HITCIRCLESZ / 2, scaleY(y) - HITCIRCLESZ / 2,HITCIRCLESZ,HITCIRCLESZ);
+    //console.log(opacity);
+    if (timing - current > 0){
+        var AC = linearScale(current, timing - mapARpreempt, mapCSr*ACSZ, timing, mapCSr);
+        c.drawImage(assets['approachCircle'], renderX(x) -  AC / 2, renderY(y) - AC / 2, AC, AC);
+    }
+    c.drawImage(assets['hitCircle'], posX, posY, mapCSr, mapCSr);
+    c.drawImage(assets['hitCircleOverlay'], posX, posY, mapCSr, mapCSr);
     c.globalAlpha = 1;
 }
 
@@ -123,22 +169,29 @@ function render() {
     
     if ((replayData.length !== 0) && (replayIdx < replayData.length - 1) && ready){
         //Hitobjects render
-        if (adjtime > mapHit[mapHitIdx][2] - mapARpreempt){
-            mapIn.push(mapHit[mapHitIdx]); //Add to list of activly drawn
-            mapHitIdx += 1;
+        if (mapHitIdx < mapHit.length){
+            if (adjtime > mapHit[mapHitIdx][2] - mapARpreempt){
+                mapIn.push(mapHit[mapHitIdx]); //Add to list of activly drawn
+                mapHitIdx += 1;
+            }
         }
+
         var i = 0;
         while (mapIn.length > 0){ //x,y,time,type
-            if (adjtime > mapIn[i][2] + mapOD5){mapIn.splice(i,1); if(i>=mapIn.length){break;} continue;} //THIS IS CORRECT CODE
-            //if (adjtime > mapIn[i][2]){mapIn.pop(i); i += 1; if(i>=mapIn.length){break;} continue;} //THIS CODE IS WRONG TODO when judgements
+            //if (adjtime > mapIn[i][2] + mapOD5){mapIn.splice(i,1); if(i>=mapIn.length){break;} continue;} //THIS IS CORRECT CODE
+            if (adjtime > mapIn[i][2]+25){mapIn.splice(i,1); if(i>=mapIn.length){break;} continue;} //THIS CODE IS WRONG TODO when judgements
+            if (mapIn[i][3]&0b100) { //New combo
+                replayCombo += 1;
+            }
             if (mapIn[i][3]&1 !== 0 || 1) { // Hitobject
                 var opacity = 1;
+                var approach = 1;
                 if (adjtime < (mapIn[i][2] - mapARfull)){
                     opacity = adjtime/(mapARpreempt-mapARfull)+(mapIn[i][2]-mapARpreempt)/(mapARfull-mapARpreempt);
                 } else {
                     opacity = 1;
                 }
-                drawHitCircle(mapIn[i][0],mapIn[i][1],opacity); //LAST PARAM IS OPACITY !
+                drawHitCircle(mapIn[i][0],mapIn[i][1], assets['hitCircle'+replayCombo%comboColorCount], opacity, adjtime, mapIn[i][2]);
             }
             i += 1;
             if (i >= mapIn.length){
@@ -162,9 +215,7 @@ function render() {
     }
     
 
-    
-    
-    
+
     //if (Date.now() - replayStartTime > replayData[replayIdx][0]){
     //    drawCursor(replayData[replayIdx][1],replayData[replayIdx][2])
     //    while (Date.now() - replayData[replayIdx][0] > replayData[replayIdx][0]) {replayIdx += 1;}
--- utils.js
+++ utils.js
@@ -48,6 +48,48 @@ function getMods(val){
     return (result === "") ? "none" : result;
 }
 
+function tintImage(imgElement,tintColor) {  return new Promise((resolve) => {
+        // create hidden canvas (using image dimensions)
+        var canvas = document.createElement("canvas");
+        canvas.width = imgElement.width;
+        canvas.height = imgElement.height;
+        
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(imgElement,0,0);
+
+        var map = ctx.getImageData(0,0,320,240);
+        var imdata = map.data;
+
+        // convert image to grayscale
+        var r,g,b,avg;
+        for(var p = 0, len = imdata.length; p < len; p+=4) {
+            r = imdata[p]
+            g = imdata[p+1];
+            b = imdata[p+2];
+            
+            avg = Math.floor((r+g+b)/3);
+
+            imdata[p] = imdata[p+1] = imdata[p+2] = avg;
+        }
+
+        ctx.putImageData(map,0,0);
+
+        // overlay filled rectangle using lighter composition
+        ctx.globalCompositeOperation = "lighter";
+        ctx.globalAlpha = 0.5;
+        ctx.fillStyle=tintColor;
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+
+        // replace image source with canvas data
+        //console.log("meow");
+        //console.log(canvas.toDataURL());
+        loadImage(canvas.toDataURL()).then(tintedImage => {
+            resolve(tintedImage)
+        });
+    
+})};
+
 function unpackUnitData(bytes, offset, sz){
     switch (sz){
     case BYTE:
@@ -109,4 +151,8 @@ function unpackString(bytes, offset){
         content = enc.decode(view);
     }
     return [sz, content];
+}
+
+function linearScale(x, x0, y0, x1, y1){
+    return x*((y0-y1)/(x0-x1))+y1-x1*((y0-y1)/(x0-x1))
 }
