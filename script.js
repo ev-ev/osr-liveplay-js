2dce030ad94cfbe5350dcc71ae30273e1640803b
Evgeny V <evgeny.singapore@gmail.com>
2023-11-19 14:24:20 +0800

    Implemented sliders (linear only, todo the 3 remaining types), including slider border+slider body+slider start+duration delay based on slider length. Pause feature. Timing point system implemented, they now affect slider duration only (todo). Fixed bug where hit objects loaded in had more elements than I was pre-processing, resulting in info missing at the render call. (+5)

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var abs=Math.abs;
+var pow=Math.pow;
+var arctan=Math.atan;
+var pi = Math.PI;
 var ready=0;
 var CURSORSZ = 70;
 var ACSZ = 4;
+var SLIDERBODYBASEOPACITY = 0.5;
+var SLIDERBORDERSIZE = 5;
+var SLIDERSZ = -7; //How much smaller slider elements should be offset than map circle size radius
 var COMBONUMSZ = 0.78;
 
 var playfieldScale = 1;
@@ -9,6 +15,8 @@ var playfieldTransformX = 0;
 var playfieldTransformY = 0;
 
 var displayText = '';
+var paused = false;
+var pauseTime = 0;
 
 var replayData = []; //TimeMS x y keypressed
 var replayIdx = 0;
@@ -16,6 +24,7 @@ var replayStartTime = 0;
 var replayTimeSum = 0;
 var replayCombo = -1;
 var currentCombo = 0;
+var timingData = [];
 var songStart;
 
 var mapHit = [];
@@ -25,6 +34,7 @@ var mapOD3 = 80;
 var mapOD1 = 140;
 var mapOD5 = 200;
 var mapCSr = 54.4;
+var mapSliderMul = 100;
 var mapARpreempt = 1200;
 var mapARfull = 800;
 var combosizeX = 1;
@@ -78,11 +88,24 @@ function setup(){
     songStart = -replayTimeSum;
     loadImage(replayValues['bg-src']).then(backgroundImage => {assets['backgroundImage'] = backgroundImage;
     loadMap(replayValues['map-src']).then(mapValues => { console.log(mapValues);
-    mapHit = mapValues['HitObjects'].map(e=>[Number(e[0]),Number(e[1]),Number(e[2]),Number(e[3]),Number(e[4]),e[5],e[6]]);
+    mapHit = mapValues['HitObjects'].map(e=>{
+    if (e.length === 6){
+        return [Number(e[0]),Number(e[1]),Number(e[2]),Number(e[3]),Number(e[4]),e[6]];
+    } else if (e.length === 7){
+        return [256, 192, Number(e[2]), Number(e[3]), Number(e[4]), Number(e[6]),e[7]];
+    } else if (e.length === 11){
+        return [Number(e[0]),Number(e[1]),Number(e[2]),Number(e[3]),Number(e[4]),e[5],Number(e[6]),Number(e[7]),e[8],e[9],e[10]]
+    } else {
+        alert(e); 
+    }
+    });
+    //mapHit = mapValues['HitObjects'];
     mapOD3 = 80 - 6 * mapValues['Difficulty']['OverallDifficulty'];
     mapOD1 = 140 - 8 * mapValues['Difficulty']['OverallDifficulty'];
     mapOD5 = 200 - 10 * mapValues['Difficulty']['OverallDifficulty'];
     mapCSr = (54.4 - 4.48 * mapValues['Difficulty']['CircleSize'])*playfieldScale*2; //mul by 2 since its the radius
+    SLIDERSZ += mapCSr;
+    mapSliderMul = mapValues['Difficulty']['SliderMultiplier'];
     var mapAR = Number(mapValues['Difficulty']['ApproachRate']);
     //var mapAR = 1;
     if (mapAR <= 5){
@@ -101,11 +124,16 @@ function setup(){
     combosizeX = mapCSr*(assets['default0'].width/assets['hitCircle'].width) * COMBONUMSZ; //Precalculate commbo number size
     combosizeY = mapCSr*(assets['default0'].height/assets['hitCircle'].height) * COMBONUMSZ; //TODO I am assuming all are same size !
     comboColorCount = comboColors.length;
+    
+    timingData = mapValues['TimingPoints'];
+    timingIdx = -1;
+    timingIdxInherited = -1;
+    
     promises = [];
     for (var i = 0; i < comboColors.length; i++) {
         promises.push(tintImage(assets['hitCircle'], comboColors[i]));
     }
-    //console.log(promises);
+    
     Promise.all(promises).then(comboColorResults => {
     for (var i = 0; i < comboColorResults.length; i++) {
         assets['hitCircle'+i] = comboColorResults[i];
@@ -143,6 +171,52 @@ function drawCursor(x, y) {
     c.drawImage(assets['cursorImage'], renderX(x) - CURSORSZ / 2, renderY(y) - CURSORSZ / 2,CURSORSZ,CURSORSZ);
 }
 
+function drawSlider(x, y, combo, opacity, current, timing, curveType, curvePoints, slides, length){
+    if (curveType === "L"){
+        c.beginPath();
+        var start = [renderX(x),renderY(y)]
+        var end = [renderX(curvePoints.slice(-1)[0][0]),renderY(curvePoints.slice(-1)[0][1])];
+        c.moveTo(start[0], start[1]);
+        c.lineTo(end[0],end[1]);
+        var clength = pow(pow((end[0]-start[0]),2)+pow((end[1]-start[1]),2),0.5);
+        if (clength<length){
+            c.lineTo((end[0]-start[0])/clength*length,(end[1]-start[1])/clength*length);
+        } else {length = clength;}
+        c.lineCap = "round";   
+        c.lineWidth = SLIDERSZ - SLIDERBORDERSIZE;
+        c.strokeStyle = 'rgba('+assets['skinIni']['Colours']['SliderTrackOverride']+','+SLIDERBODYBASEOPACITY*opacity+')';
+        c.stroke();
+        c.closePath();
+        
+        
+        c.save();
+        c.translate(start[0], start[1]);
+        if (end[0] > start[0]) { //I don't have enough braincells to figure out why this is a thing
+            c.rotate(arctan((end[1]-start[1])/(end[0]-start[0]))-pi/2);
+        } else {
+            c.rotate(arctan((end[1]-start[1])/(end[0]-start[0]))+pi/2);
+        }
+        c.beginPath();
+        c.moveTo(-SLIDERSZ / 2, 0);
+        c.lineTo(-SLIDERSZ / 2, length*playfieldScale);
+        c.arc   ( 0, length*playfieldScale, SLIDERSZ / 2, pi, 0, true);
+        //c.moveTo( SLIDERSZ / 2, length*playfieldScale);
+        c.lineTo( SLIDERSZ / 2, 0);
+        c.arc   ( 0, 0, SLIDERSZ / 2, 0, pi, true);
+        //c.moveTo(-SLIDERSZ / 2, 0);
+        c.lineCap = "butt"; 
+        c.lineWidth = SLIDERBORDERSIZE;
+        c.strokeStyle = 'rgba('+assets['skinIni']['Colours']['SliderBorder']+','+opacity+')'; //SliderTrackOverride
+        //c.strokeRect(-SLIDERSZ / 2,0,SLIDERSZ,length*playfieldScale);
+        c.stroke();
+        c.closePath();
+        c.restore();
+        
+        
+    }
+    drawHitCircle(x, y, combo, opacity, current, timing);
+}
+
 function drawHitCircle(x, y, combo, opacity, current, timing){
     var posX = renderX(x) - mapCSr / 2;
     var posY = renderY(y) - mapCSr / 2;
@@ -169,6 +243,17 @@ function drawHitCircle(x, y, combo, opacity, current, timing){
     c.globalAlpha = 1;
 }
 
+function pause(){
+    if (!paused){
+        paused = true;
+        pauseTime = Date.now();
+    } else {
+        replayStartTime += Date.now()-pauseTime;
+        paused = false;
+    }
+
+}
+
 function render() {
     if (assets['backgroundImage']){c.drawImage(assets['backgroundImage'], 0, 0, w, h);}
     c.fillStyle = 'rgba(17, 17, 17, 0.7)';
@@ -176,10 +261,22 @@ function render() {
     c.fillStyle = 'rgba(195, 195, 195, 0.7)'
     c.fillText(displayText,10,10);
     
-    ctime = Date.now() - replayStartTime;
+    if (!paused){
+        ctime = Date.now() - replayStartTime;
+    }
     adjtime = ctime - songStart;
     
     if ((replayData.length !== 0) && (replayIdx < replayData.length - 1) && ready){
+        //TimingPoints loading
+        /*if (timingIdxInherited - 1 < timingData.length){
+            if (adjtime > timingData[timingIdxInherited + 1]) {
+                if (timingData[timingIdxInherited + 1][6]) {
+                    timingIdx += 1;
+                }
+                
+                timingIdxInherited += 1;
+            }
+        }*/
         //Hitobjects render
         if (mapHitIdx < mapHit.length){
             if (adjtime > mapHit[mapHitIdx][2] - mapARpreempt){
@@ -194,21 +291,47 @@ function render() {
 
         var i = 0;
         while (mapIn.length > 0){ //x,y,time,type
-            //if (adjtime > mapIn[i][2] + mapOD5){mapIn.splice(i,1); if(i>=mapIn.length){break;} continue;} //THIS IS CORRECT CODE
-            if (adjtime > mapIn[i][2]+25){mapIn.splice(i,1); if(i>=mapIn.length){break;} continue;} //THIS CODE IS WRONG TODO when judgements
-            //if (mapIn[i][3]&0b100) { //New combo
-            //    replayCombo += 1; //This straight up won't work TODO
-            //    currentCombo = 1;
-            //}
-            if (mapIn[i][3]&1 !== 0 || 1) { // Hitobject
+            var timingIdx = 0; var timingIdxInherited = 0;
+            for (var j = 0; j < timingData.length; j++) { //This bit goes thru timingData and selects the appropriate one for the hitobject in question. might just tbe the most fucked up thing ive made yet
+                if (mapIn[i][2] < timingData[j][0]) {   //TODO this can be impoved regarding the starting position of the variable, start from the timing point that doesnt exist anymore
+                    timingIdxInherited = j - 1;
+                    break;
+                }
+                if (timingData[j][6] === "1") {
+                    timingIdx = j;
+                    timingIdxInherited = j;
+                }
+            }
+            if ((mapIn[i][3]&1) !== 0) { // Hitobject
+                if (adjtime > mapIn[i][2]+25){mapIn.splice(i,1); if(i>=mapIn.length){break;} continue;}
                 var opacity = 1;
-                var approach = 1;
                 if (adjtime < (mapIn[i][2] - mapARfull)){
                     opacity = adjtime/(mapARpreempt-mapARfull)+(mapIn[i][2]-mapARpreempt)/(mapARfull-mapARpreempt);
                 } else {
                     opacity = 1;
                 }
-                drawHitCircle(mapIn[i][0],mapIn[i][1], mapIn[i][7], opacity, adjtime, mapIn[i][2]);
+                drawHitCircle(mapIn[i][0],mapIn[i][1], mapIn[i].slice(-1)[0], opacity, adjtime, mapIn[i][2]);
+            }
+            if ((mapIn[i][3]&2) !== 0){ // Slider x,y,time,type,hitSound,curveType|curvePoints,slides,length,edgeSounds,edgeSets,hitSample
+                var duration = 0;
+                if (timingIdx === timingIdxInherited) { //Not inherited
+                    duration = mapIn[i][7] / (mapSliderMul * 100) * timingData[timingIdx][1];
+                } else {
+                    duration = mapIn[i][7] / (mapSliderMul * (-timingData[timingIdxInherited][1])) * timingData[timingIdx][1];
+                }
+                
+                if (adjtime > mapIn[i][2] + duration){mapIn.splice(i,1); if(i>=mapIn.length){break;} continue;}
+                var opacity = 1;
+                if (adjtime < (mapIn[i][2] - mapARfull)){
+                    opacity = adjtime/(mapARpreempt-mapARfull)+(mapIn[i][2]-mapARpreempt)/(mapARfull-mapARpreempt);
+                } else {
+                    opacity = 1;
+                }
+                //var [curveType, curvePoints] = mapIn[i][5].split("|");
+                var curvePoints = mapIn[i][5].split('|');
+                curveType = curvePoints.splice(0,1)[0]
+                curvePoints = curvePoints.map(x=>x.split(":").map(y=>Number(y)));
+                drawSlider(mapIn[i][0],mapIn[i][1], mapIn[i].slice(-1)[0], opacity, adjtime, mapIn[i][2], curveType, curvePoints, Number(mapIn[i][6]), Number(mapIn[i][7]));
             }
             i += 1;
             if (i >= mapIn.length){
